c0ec7041fef2e29818a8a96fae4bef873f1887d8
Chris Johnson <cpjohnson@verisk.com>
2018-11-07 07:26:14 -0700

    Add configurations to build both debug and release
    Still learning CMake.
    1. It's not necessary to set the CMAKE_BINARY_DIR. So I'm removing it.
    2. In the build.bat file, I build both debug and release to demonstrate and learn how it's done.
    3. Reduce the size the of the .gitignore file.

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
 @echo off
-cmake --build bin\CMakeFiles -- -v:m -nologo
+cmake --build bin\CMakeFiles --config debug -- -v:m -nologo
+cmake --build bin\CMakeFiles --config release -- -v:m -nologo
 @echo on
