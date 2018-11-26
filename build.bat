039b1338e03b5194d1acee799ab79577c480e267
Chris Johnson <cpjohnson@verisk.com>
2018-11-26 11:43:28 -0700

    Add install target
    I needed to learn about the cmake install function.
    So I added this minimal code to learn more about it.

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @echo off
-cmake --build bin\CMakeFiles --config debug -- -v:m -nologo
-cmake --build bin\CMakeFiles --config release -- -v:m -nologo
+cmake --build bin\CMakeFiles --target install --config debug -- -v:m -nologo
+cmake --build bin\CMakeFiles --target install --config release -- -v:m -nologo
 @echo on
